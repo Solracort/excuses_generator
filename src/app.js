1516ce4e9054384ce726a95fde4bc215bada160f
Solracort <solracort@gmail.com>
2022-11-15 19:36:30 +0000

    Commit 0.1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 };
