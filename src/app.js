5e9420cdaa1ef830ccd931cb6f5f1c391c064bad
Solracort <solracort@gmail.com>
2022-11-18 18:04:00 +0000

    Finished Version

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+/*window.onload = */
 
+boton.onclick = function otra() {
+  const excuseHTML = document.querySelector("#excuse");
+  let i,
+    j,
+    k = 0;
   let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
+  let what = [" ate ", " pissed ", " crushed ", " broked "];
   let when = [
     "before the class",
     "right in time",
@@ -17,4 +21,10 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
+  i = Math.floor(Math.random() * 4);
+  j = Math.floor(Math.random() * 4);
+  k = Math.floor(Math.random() * 5);
+  let excuse = who[i] + what[j] + when[k];
+  excuseHTML.innerHTML = excuse;
+  return excuse;
 };
